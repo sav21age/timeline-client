5992cadc1404a2aa974322b527c35c7b2f4f9cdb
anton <sav21age@gmail.com>
2023-02-18 15:34:21 +0300

    picture test

--- src/components/Picture.tsx
+++ src/components/Picture.tsx
@@ -10,14 +10,20 @@ type Props = {
   height?: string,
 };
 
+const replaceSlashes = (path: string) => {
+  let t = 0
+  return path.replace(/\/\//g, match => ++t >= 2 ? '/' : match)
+}
+
 const Picture = (props: Props) => {
   // const fallBackSrc = `${process.env.PUBLIC_URL}/media/svg/none.svg`;
   return (
     <img
-      src={`${props.path}`.replace(
-        "//",
-        "/"
-      )}
+      // src={`${props.path}`.replace(
+      //   "//",
+      //   "/"
+      // )}
+      src={replaceSlashes(props.path)}
       onError={e => (e.currentTarget.src = (props.pathFallback === undefined ? appConfig.FALLBACK_SRC : props.pathFallback))}
       alt={props.alt}
       className={props.className}
